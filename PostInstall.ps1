f26a3dcd64c7154e1f1b2e94c71175fd378f2891
Marsu5 <marcell.schuh@gmail.com>
2017-07-27 19:09:06 +0200

    Fully automated

--- PostInstall.ps1
+++ PostInstall.ps1
@@ -1,1 +1,14 @@
-# POST INSTALL
+function setupprofile () {
+    if(Test-Path $HOME\Documents\WindowsPowerShell\){
+        Copy-Item .\Microsoft.PowerShell-profile.ps1 $HOME\Documents\WindowsPowerShell\profile.ps1
+    }else{
+        mkdir $HOME\Documents\WindowsPowerShell\
+        setupprofile
+    }
+}
+
+Write-Output "Seting up powershell profile."
+setupprofile
+Copy-Item .\PostInstall.ps1 $env:temp\postinstall.ps1
+Write-Output "Installation complete. Press any key to quit ..."
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
